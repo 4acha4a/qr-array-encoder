7d4e3e3ce5b2076dfc6500778b43a66c7979e81b
4acha4a <57715735+4acha4a@users.noreply.github.com>
2023-09-06 13:59:08 +0300

    Python script and example file added

---
+++ image-to-grayscale.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import sys
+
+def image_to_grayscale_array(image_path):
+    # Open the image file
+    with Image.open(image_path) as img:
+        # Convert image to grayscale
+        grayscale_img = img.convert('L')
+        grayscale_img = grayscale_img.resize((600, 600))
+        
+        # Get image data as a list of lists (2D array)
+        img_data = list(grayscale_img.getdata())
+        grayscale_array = [img_data[n:n+grayscale_img.width] for n in range(0, len(img_data), grayscale_img.width)]
+        
+        return grayscale_array
+
+if __name__ == "__main__":
+    # if len(sys.argv) < 2:
+    #     print("Usage: python3 image_to_grayscale_array.py <path_to_image>")
+    #     sys.exit(1)
+
+    image_path = sys.argv[1]
+    grayscale_array = image_to_grayscale_array(image_path)
+
+    # Print the grayscale array to stdout
+    print(grayscale_array)
